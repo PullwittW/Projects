b785a6feec169e35e75ca3b03240d9b53d476001
PullwittWangu <wangupullwitt.priv@gmail.com>
2022-05-23 16:15:06 +0200

    Release Update

--- hangman.py
+++ hangman.py
@@ -8,7 +8,7 @@
 # Einführungstext
 print("\n")
 print("Willkommen zum Hangman-Spiel! \n" "Das Spiel besteht darin, ein Wort welches von einem Computer generiert wird in einer bestimmten Anzahl von Versuchen zu erraten. \n" "Wenn du das Wort erraten hast, dann hast du gewonnen! \n" "Wenn du das Wort nicht erraten hast, dann hast du verloren! \n" "Viel Glück! \n")
-print("Die Schwierigkeitsstufe, sprich die Anzahl der Versuche, errechnet sich aus der länge des Wortes bei 'schwer', \n der länge des Wortes + 3 bei 'mittel' und der länge des Wortes + 6 bei 'leicht'")
+print("Die Schwierigkeitsstufe, sprich die Anzahl der Versuche, errechnet sich aus der länge des Wortes bei 'schwer', \nder länge des Wortes + 3 bei 'mittel' und der länge des Wortes + 6 bei 'leicht'")
 print("\n")
 # Wörterbuch 'woerter.txt' lesen und ein Wort mit mindestens 5 Buchstaben zufällig auswählen
 import random
@@ -37,6 +37,7 @@ elif schwierigkeit == "2":
     schwierigkeit = len(definedWord) + 3    # medium
 elif schwierigkeit == "3":
     schwierigkeit = len(definedWord)        # hart
+print("\n")       
 
 # erstellt die erste Linie mit '_' des Wortes
 def createLines():
@@ -68,21 +69,25 @@ def newLine():
 createLines()
 
 # Schleife, welche die Schwierigkeit berücksichtigt und ob das Wort schon vollständig erraten wurde
-while anfrageRundenzahl <= schwierigkeit or len(listOfInput) == len(definedWord):
+while anfrageRundenzahl  <= schwierigkeit  or len(listOfInput) == len(definedWord):
     
     # Eingabe des Buchstabens
-    inputChar = input("Buchstabe: ")
+    inputChar = input("Buchstabe eingeben: ")
     # checken, ob der Buchstabe im Wort ist
     check()
     # Liste der falschen Buchstaben ausgeben (Hilfestellung zum erinnern an die bereits eingegebenen Buchstaben)
     if len(listOfWrongInput) != 0:
-        print('Liste der falschen Buchstaben:' , listOfWrongInput[:])
+        print('Liste der falschen Buchstaben:' , listOfWrongInput[:], "\n")
     # Aufruf der Funktion zum Erstellen einer neuen Linie
     newLine()
 
     # Ausgabe der Anzahl der Versuche und der noch zu verfügungstehenden
     print('Versuche: ', anfrageRundenzahl, 'von', schwierigkeit)
     anfrageRundenzahl += 1
+    print("--------------------------------------------------------------------------------")
+    if anfrageRundenzahl == schwierigkeit :
+        print("\n")
+        print("Du hast die letzte Chance!\n")
 
 # Ausgabe des Wortes, wenn das Wort nicht erraten wurde
 if listOfInput == definedWord:
